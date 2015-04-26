c96aed6851789f76b23fcd24d2fb2cd107ff6110
Sym Roe <sym.roe@talusdesign.co.uk>
2015-04-26 20:33:47 +0100

    Add research stats

--- democracy_club/apps/research/urls.py
+++ democracy_club/apps/research/urls.py
@@ -3,11 +3,13 @@ from django.conf.urls import patterns, url, include
 from rest_framework import routers
 
 from .api import AnswerView
+from .views import StatsView
 
 router = routers.DefaultRouter()
 router.register(r'answers', AnswerView)
 
 
 urlpatterns = patterns('',
+    url(r'stats', StatsView.as_view()),
     url(r'', include(router.urls)),
 )
