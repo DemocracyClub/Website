e5738e6ba58285c9cdf14f0c5b183dd7c35e5e55
Sym Roe <sym.roe@talusdesign.co.uk>
2015-04-30 19:43:56 +0100

    Add signup form and API endpoint

--- democracy_club/apps/dc_members/urls.py
+++ democracy_club/apps/dc_members/urls.py
@@ -1,6 +1,13 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 
+from rest_framework import routers
+
+from .api import MemberView
 from . import views
+
+router = routers.DefaultRouter()
+router.register(r'members', MemberView)
+
 
 urlpatterns = patterns('',
     url(r'^redirect/(?P<url>.*)', views.MemberLinkRedirectView.as_view()),
@@ -10,4 +17,5 @@ urlpatterns = patterns('',
         name='view_member'),
     url(r'^token/(?P<token>.*)/$', views.MemberLoginFromTokenView.as_view(),
             name='member_token_login'),
+    url(r'^api/', include(router.urls)),
 )
