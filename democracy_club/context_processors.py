9da474cdbfd630b662e4c38142a1188d8f5b087a
Sym Roe <sym.roe@talusdesign.co.uk>
2015-05-07 00:36:10 +0100

    Remove count down

--- democracy_club/context_processors.py
+++ democracy_club/context_processors.py
@@ -1,7 +1,7 @@
 import datetime
 
 def days_to_election(request):
-    end_timestamp = 1430956800
+    end_timestamp = 1430959200
     end_date = datetime.datetime.fromtimestamp(end_timestamp)
     if datetime.datetime.now() < end_date:
         delta = end_date - datetime.datetime.now()
