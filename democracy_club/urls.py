3287c2ddc36f0fd6b40af2e3e717b34a6c3be797
Sym Roe <sym.roe@talusdesign.co.uk>
2015-08-23 16:22:23 +0100

    Post election copy

--- democracy_club/urls.py
+++ democracy_club/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
+from core.views import HomeView
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -16,13 +18,12 @@ urlpatterns = patterns('',
     (r'^accounts/', include('allauth.urls')),
     url(r'^logout/$', 'django.contrib.auth.views.logout',
         {'next_page': '/'}, name='auth_logout'),
-    url(r'^$', TemplateView.as_view(template_name="home.html"), name="home"),
+    url(r'^$', HomeView.as_view(), name="home"),
     url(r'thanks/$', TemplateView.as_view(template_name="thanks.html")),
     url(r'thanks/finished$', TemplateView.as_view(template_name="thanks_finished.html")),
     url(r'^about/$', TemplateView.as_view(template_name="about.html")),
-    url(r'^data/$', TemplateView.as_view(template_name="data.html")),
+    url(r'^projects/$', TemplateView.as_view(template_name="projects.html")),
     url(r'^contact/$', TemplateView.as_view(template_name="contact.html")),
-    url(r'^help_out/$', TemplateView.as_view(template_name="help_out.html")),
     url(r'^members/', include('dc_members.urls')),
     url(r'^blog/', include('hermes.urls')),
     url(r'^research/', include('research.urls', namespace="research")),
