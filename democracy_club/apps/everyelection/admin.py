7f4a71399aa811a494b1c83c069b605f4a42a828
Sym Roe <sym.roe@talusdesign.co.uk>
2016-02-10 19:15:12 +0000

    Add AuthorityElectionSkipped to Django admin

--- democracy_club/apps/everyelection/admin.py
+++ democracy_club/apps/everyelection/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import AuthorityElection, AuthorityElectionPosition
+from .models import (AuthorityElection, AuthorityElectionPosition,
+                     AuthorityElectionSkipped)
 
+class AuthorityElectionSkippedAdmin(admin.ModelAdmin):
+    list_display = ('authority_election', 'user', 'notes')
 
 admin.site.register(AuthorityElection)
 admin.site.register(AuthorityElectionPosition)
+admin.site.register(AuthorityElectionSkipped, AuthorityElectionSkippedAdmin)
