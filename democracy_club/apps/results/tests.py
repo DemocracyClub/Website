a1189daf47b0c765669ba7569659e7dc1b7a2eb2
Sym Roe <sym.roe@talusdesign.co.uk>
2017-03-06 16:06:45 +0000

    Remove the Every Election app

--- democracy_club/apps/results/tests.py
+++ democracy_club/apps/results/tests.py
@@ -1,15 +1,1 @@
-from django.test import TestCase
 
-from everyelection.models import AuthorityElection
-
-
-class ResultsRecordingTestCase(TestCase):
-    def setUp(self):
-        self.election = AuthorityElection.objects.create(
-            election_id="local.test.2016.05.05",
-            authority_id=1,
-            election_date="2016-05-05",
-            percent_posts="30"
-        )
-
-    # def test_
