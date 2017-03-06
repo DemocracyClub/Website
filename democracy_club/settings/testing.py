51b8c677c0e36dfba8d2275c32e21c473cd5b57a
Sym Roe <sym.roe@talusdesign.co.uk>
2017-03-06 17:18:31 +0000

    DB settings for testing

--- democracy_club/settings/testing.py
+++ democracy_club/settings/testing.py
@@ -1,1 +1,14 @@
 from .base import *  # noqa
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'dc_website_test',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+
