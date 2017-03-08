26e987ac7d9fe5aac5f8a2d05d6c5a752818463a
Sym Roe <sym.roe@talusdesign.co.uk>
2017-03-08 13:43:03 +0000

    Mask key and token from trello requests

--- democracy_club/settings/testing.py
+++ democracy_club/settings/testing.py
@@ -1,19 +1,5 @@
 from .base import *  # noqa
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'dc_website_test',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    }
-}
-
-
-
-BACKLOG_TRELLO_BOARD_ID = "empty"
-BACKLOG_TRELLO_DEFAULT_LIST_ID = "empty"
 BACKLOG_TRELLO_KEY = "empty"
 BACKLOG_TRELLO_TOKEN = "empty"
+
