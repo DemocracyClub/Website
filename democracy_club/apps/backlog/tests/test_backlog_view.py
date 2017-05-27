635a62216d858df904c8701722528a108bf99a07
Sym Roe <sym.roe@talusdesign.co.uk>
2017-05-27 13:00:49 +0100

    Fix backlog tests

--- democracy_club/apps/backlog/tests/test_backlog_view.py
+++ democracy_club/apps/backlog/tests/test_backlog_view.py
@@ -15,5 +15,6 @@ class TestBacklogView(TestCase):
 
     def test_info_on_backlog_cards(self):
         resp = self.client.get('/quests/')
-        self.assertContains(resp, "Time required: <strong>10 minutes")
+        assert resp.status_code == 200
+        self.assertContains(resp, self.card.time_required)
         self.assertContains(resp, self.card.title)
