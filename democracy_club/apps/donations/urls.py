aaa5975726934031ac20b96130428a56d4c736a6
Sym Roe <sym.roe@talusdesign.co.uk>
2017-06-06 12:43:41 +0100

    Deal with GC confirmation hook

--- democracy_club/apps/donations/urls.py
+++ democracy_club/apps/donations/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import DonateFormView
+from .views import DonateFormView, DonateThanksView
 
 urlpatterns = [
+    url(r'thanks', DonateThanksView.as_view(), name="donate_thanks"),
     url(r'', DonateFormView.as_view(), name="donate"),
 ]
