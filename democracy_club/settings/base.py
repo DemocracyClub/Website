af481d40a3881c79a8f3aee8eabf3e7dffe64df4
chris48s <chris.shaw480@gmail.com>
2018-07-25 14:55:14 +0100

    remove django.contrib.gis from INSTALLED_APPS
    Removing this stops us from throwing
    
    OSError: /var/task/libgeos_c.so:
    cannot open shared object file:
    No such file or directory
    
    when we deploy
    
    We don't need to use any of the
    contrib.gis stuff in this project.

--- democracy_club/settings/base.py
+++ democracy_club/settings/base.py
@@ -86,7 +86,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'django.contrib.gis',
     'localflavor',
     'markdown_deux',
     'django_extensions',
