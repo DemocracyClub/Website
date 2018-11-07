adb40c69c3455d99b908f2de7a457f7a688acb68
Sym Roe <sym.roe@talusdesign.co.uk>
2018-11-07 14:40:35 +0000

    Invalidate cloudfront cache when saving blog posts

---
+++ democracy_club/apps/core/cloudfront.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+import boto3
+
+from django.conf import settings
+
+def invalidate_paths(path_list):
+    if not hasattr(settings, 'ZAPPA_STAGE'):
+        return
+    cloudfront = boto3.client('cloudfront')
+
+    cloudfront.create_invalidation(
+        DistributionId=settings.AWS_CLOUD_FRONT_ID,
+        InvalidationBatch={
+            'Paths': {
+                'Quantity': len(path_list),
+                'Items': path_list
+            },
+            'CallerReference': 'django-invalidate-{}'.format(datetime.now())
+        }
+    )
+
