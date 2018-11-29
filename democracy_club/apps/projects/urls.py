4246b11183473625c41aa8cd7d7513d4960179d5
Sym Roe <sym.roe@talusdesign.co.uk>
2018-11-29 11:25:49 +0000

    Add projects app

--- democracy_club/apps/projects/urls.py
+++ democracy_club/apps/projects/urls.py
@@ -6,17 +6,27 @@ urlpatterns = [
     url(
         r"^$",
         TemplateView.as_view(template_name="projects/projects_home.html"),
-        name="projects",
+        name="home",
     ),
     url(
-        r"^projects/polling-stations/$",
+        r"^past/$",
+        TemplateView.as_view(template_name="projects/past.html"),
+        name="past",
+    ),
+    url(
+        r"^reports/$",
+        TemplateView.as_view(template_name="projects/reports_home.html"),
+        name="reports",
+    ),
+    url(
+        r"^polling-stations/$",
         TemplateView.as_view(
             template_name="projects/polling-stations/home.html"
         ),
         name="polling_one_pager",
     ),
     url(
-        r"^projects/polling-stations/technical/$",
+        r"^polling-stations/technical/$",
         RedirectView.as_view(url=reverse_lazy("polling_data_upload")),
         name="polling_technical_explainer",
     ),
@@ -26,33 +36,59 @@ urlpatterns = [
         name="polling_faqs",
     ),
     url(
-        r"^projects/polling-stations/embed/$",
+        r"^polling-stations/embed/$",
         TemplateView.as_view(
             template_name="projects/polling-stations/embed_code.html"
         ),
         name="polling_embed_code",
     ),
     url(
-        r"^projects/polling-stations/upload/$",
+        r"^polling-stations/upload/$",
         TemplateView.as_view(
             template_name="projects/polling-stations/upload_data.html"
         ),
         name="polling_data_upload",
     ),
     url(
-        r"^projects/polling-stations/techincal/$",
+        r"^polling-stations/techincal/$",
         RedirectView.as_view(url=reverse_lazy("polling_technical_explainer")),
     ),
     url(
-        r"^projects/election-ids/reference/$",
+        r"^election-ids/reference/$",
         RedirectView.as_view(
             url="https://elections.democracyclub.org.uk/reference_definition"
         ),
         name="election_ids_reference",
     ),
     url(
-        r"^projects/election-ids/$",
-        TemplateView.as_view(template_name="projects/election-ids/home.html"),
+        r"^election-ids/$",
+        TemplateView.as_view(template_name="projects/election-ids.html"),
         name="election_ids",
     ),
+    url(
+        r"^(?i)who-can-i-vote-for/$",
+        TemplateView.as_view(template_name="projects/whocanivotefor.html"),
+        name="whocanivotefor",
+    ),
+    url(
+        r"^candidates-wiki/$",
+        TemplateView.as_view(template_name="projects/candidates.html"),
+        name="candidates",
+    ),
+    url(
+        r"^data/$",
+        TemplateView.as_view(template_name="projects/data.html"),
+        name="data",
+    ),
+    url(
+        r"^(?i)election-leaflets/$",
+        TemplateView.as_view(template_name="projects/electionleaflets.html"),
+        name="election_leaflets",
+    ),
+    url(
+        r"^(?i)csv/$",
+        TemplateView.as_view(template_name="projects/cvs.html"),
+        name="cvs",
+    ),
+
 ]
