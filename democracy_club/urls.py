ca2616ec759feeeafc6c8ea98bdfd141ec7b148a
chris48s <chris.shaw480@gmail.com>
2019-08-23 17:16:00 +0100

    update admin url include syntax

--- democracy_club/urls.py
+++ democracy_club/urls.py
@@ -18,7 +18,7 @@ handler500 = 'dc_theme.urls.dc_server_error'
 
 urlpatterns = [
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^logout/$', auth_views.logout,
         {'next_page': '/'}, name='auth_logout'),
