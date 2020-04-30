80dc5c664f7b142720178d99d0db96b1c8fda97e
Sym Roe <sym.roe@talusdesign.co.uk>
2020-04-30 11:51:56 +0100

    Address all warnings

--- democracy_club/apps/projects/urls.py
+++ democracy_club/apps/projects/urls.py
@@ -1,106 +1,106 @@
-from django.urls import re_path
+from django.urls import path
 from django.urls import reverse_lazy
 from django.views.generic import RedirectView, TemplateView
 
 app_name = "projects"
 
 urlpatterns = [
-    re_path(
-        r"^$",
+    path(
+        "",
         TemplateView.as_view(template_name="projects/projects_home.html"),
         name="home",
     ),
-    re_path(
-        r"^past/$",
+    path(
+        "past/",
         TemplateView.as_view(template_name="projects/past.html"),
         name="past",
     ),
-    re_path(
-        r"^reports/$",
+    path(
+        "reports/",
         TemplateView.as_view(template_name="projects/reports_home.html"),
         name="reports",
     ),
-    re_path(
-        r"^reports/registers/$",
+    path(
+        "reports/registers/",
         TemplateView.as_view(
             template_name="projects/report_odi_registers.html"
         ),
         name="reports_registers",
     ),
-    re_path(
-        r"^polling-stations/$",
+    path(
+        "polling-stations/",
         TemplateView.as_view(
             template_name="projects/polling-stations/home.html"
         ),
         name="polling_one_pager",
     ),
-    re_path(
-        r"^polling-stations/technical/$",
+    path(
+        "polling-stations/technical/",
         RedirectView.as_view(url=reverse_lazy("polling_data_upload")),
         name="polling_technical_explainer",
     ),
-    re_path(
-        r"^projects/polling-stations/faqs/$",
+    path(
+        "projects/polling-stations/faqs/",
         RedirectView.as_view(url=reverse_lazy("projects:polling_one_pager")),
         name="polling_faqs",
     ),
-    re_path(
-        r"^polling-stations/embed/$",
+    path(
+        "polling-stations/embed/",
         TemplateView.as_view(
             template_name="projects/polling-stations/embed_code.html"
         ),
         name="polling_embed_code",
     ),
-    re_path(
-        r"^polling-stations/upload/$",
+    path(
+        "polling-stations/upload/",
         TemplateView.as_view(
             template_name="projects/polling-stations/upload_data.html"
         ),
         name="polling_data_upload",
     ),
-    re_path(
-        r"^polling-stations/techincal/$",
+    path(
+        "polling-stations/techincal/",
         RedirectView.as_view(url=reverse_lazy("projects:polling_technical_explainer")),
     ),
-    re_path(
-        r"^election-ids/reference/$",
+    path(
+        "election-ids/reference/",
         RedirectView.as_view(
             url="https://elections.democracyclub.org.uk/reference_definition"
         ),
         name="election_ids_reference",
     ),
-    re_path(
-        r"^election-ids/$",
+    path(
+        "election-ids/",
         TemplateView.as_view(template_name="projects/election-ids.html"),
         name="election_ids",
     ),
-    re_path(
-        r"^who-can-i-vote-for/$(?i)",
+    path(
+        "who-can-i-vote-for/",
         TemplateView.as_view(template_name="projects/whocanivotefor.html"),
         name="whocanivotefor",
     ),
-    re_path(
-        r"^election-widget/$",
+    path(
+        "election-widget/",
         TemplateView.as_view(template_name="projects/election-widget.html"),
         name="election_widget",
     ),
-    re_path(
-        r"^candidates-wiki/$",
+    path(
+        "candidates-wiki/",
         TemplateView.as_view(template_name="projects/candidates.html"),
         name="candidates",
     ),
-    re_path(
-        r"^data/$",
+    path(
+        "data/",
         TemplateView.as_view(template_name="projects/data.html"),
         name="data",
     ),
-    re_path(
-        r"^election-leaflets/$(?i)",
+    path(
+        "election-leaflets/",
         TemplateView.as_view(template_name="projects/electionleaflets.html"),
         name="election_leaflets",
     ),
-    re_path(
-        r"^csv/$(?i)",
+    path(
+        "csv/",
         TemplateView.as_view(template_name="projects/cvs.html"),
         name="cvs",
     ),
