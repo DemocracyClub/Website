21d524d2dba36b40f3339ad5f27b84b7748479d5
Virginia Dooley <vcdooley@gmail.com>
2021-06-10 13:17:06 +0100

    Remove unusd js

--- democracy_club/assets/js/global.js
+++ democracy_club/assets/js/global.js
@@ -1,25 +1,6 @@
 //
 //  Global Javascript for Democracy Club website
 //
-var dClub = dClub || {};
-
-// Mobile menu button: https://www.jamestease.co.uk/blether/add-remove-or-toggle-classes-using-vanilla-javascript
-dClub.nav = (function() {
-    function mobileMenuToggle() {
-
-        document.querySelector('.js-nav-site-ctrl').addEventListener('click', function(e) {
-            e.preventDefault();
-
-            [].map.call(document.querySelectorAll('.js-nav-site'), function(el) {
-                el.classList.toggle('nav-site--active');
-            });
-        });
-    }
-
-    return {
-        mobileMenuToggle: mobileMenuToggle
-    };
-})();
 
 // Check for Javscript support: https://www.jamestease.co.uk/blether/add-remove-or-toggle-classes-using-vanilla-javascript
 dClub.helpers = (function() {
