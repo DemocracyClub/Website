d513ab9f8df841d394fec384abf27eed72ad8622
Virginia Dooley <vcdooley@gmail.com>
2021-07-07 13:57:09 +0100

    Convert 2021 to new report system

--- democracy_club/apps/report_2021/urls.py
+++ democracy_club/apps/report_2021/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
+from core.report_helpers.views import MarkdownFileView
+
 app_name = "report_2021"
 
 urlpatterns = [
     url(
         r"^$",
-        TemplateView.as_view(template_name="report_2021/report.html"),
+        MarkdownFileView.as_view(markdown_file="apps/report_2021/report.md"),
         name="report_2021",
     )
 ]
