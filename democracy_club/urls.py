48de3efe2fc1fca53e600d65a79c7afbd825aae9
Sym Roe <sym.roe@talusdesign.co.uk>
2021-09-02 15:50:56 +0100

    A whole bucket'o'changes related to the website update

--- democracy_club/urls.py
+++ democracy_club/urls.py
@@ -44,9 +44,9 @@ urlpatterns = [
         name="jobs",
     ),
     path(
-        "about/funding/",
-        TemplateView.as_view(template_name="about/funding.html"),
-        name="funding",
+        "about/impact/",
+        TemplateView.as_view(template_name="about/impact.html"),
+        name="impact",
     ),
     path(
         "about/team/",
@@ -64,6 +64,7 @@ urlpatterns = [
         name="coc",
     ),
     path("projects/", include("projects.urls", "projects")),
+    path("support-us/", TemplateView.as_view(template_name="support_us.html"), name="support_us"),
     path(
         "contact/",
         TemplateView.as_view(template_name="contact.html"),
@@ -95,14 +96,25 @@ urlpatterns = [
         ),
     ),
     path(
-        "data/",
-        RedirectView.as_view(url=reverse_lazy("projects")),
-    ),
-    path(
         "mailing_list/",
         include(("mailing_list.urls", "dc_signup_form")),
     ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Tuple of URLs to redirect to. Maintained for backwards compatibility / old links
+url_redirects = (
+    path(
+        "data/",
+        RedirectView.as_view(url=reverse_lazy("projects")),
+    ),
+    path(
+        "about/funding/",
+        RedirectView.as_view(url=reverse_lazy("support_us")),
+        name="funding",
+    ),
+)
+
+urlpatterns += url_redirects
 
 
 if settings.DEBUG:
