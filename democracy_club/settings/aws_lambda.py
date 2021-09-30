e6e6d876197ec3e0ece131e5094ac78b93534c41
Sym Roe <sym.roe@talusdesign.co.uk>
2021-09-30 15:00:24 +0100

    Public access

--- democracy_club/settings/aws_lambda.py
+++ democracy_club/settings/aws_lambda.py
@@ -3,9 +3,10 @@ import os
 from .base import setup_sentry
 from .base import *  # noqa
 
-FORCE_SCRIPT_NAME = "/"
+if os.environ.get("APP_IS_BEHIND_CLOUDFRONT", False):
+    FORCE_SCRIPT_NAME = "/"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [os.environ.get("FQDN")]
 
 # Override the database name and user if needed
 DATABASES = {
@@ -29,35 +30,21 @@ STATICFILES_FINDERS = (
 )
 
 AWS_S3_SECURE_URLS = True
-AWS_S3_HOST = "s3-eu-west-1.amazonaws.com"
+AWS_S3_HOST = "s3-eu-west-2.amazonaws.com"
 AWS_S3_USE_SSL = False
 AWS_S3_REGION_NAME = "eu-west-2"
 
-# Stage
-AWS_STORAGE_BUCKET_NAME = "static.dev.democracyclub.org.uk"
-AWS_S3_CUSTOM_DOMAIN = "static-dev.democracyclub.org.uk"
-
-# # Prod
-# AWS_STORAGE_BUCKET_NAME = "static.democracyclub.org.uk"
-# AWS_S3_CUSTOM_DOMAIN = "static.democracyclub.org.uk"
+AWS_STORAGE_BUCKET_NAME = os.environ.get("STORAGE_BUCKET_NAME")
+AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME
 
 MEDIAFILES_LOCATION = "media"
 MEDIA_URL = "https://{}/{}/".format(AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 DEFAULT_FILE_STORAGE = "core.s3_lambda_storage.MediaStorage"
 
-CSRF_TRUSTED_ORIGINS = [
-    "stage.democracyclub.org.uk",
-    "democracyclub.org.uk",
-]
+CSRF_TRUSTED_ORIGINS = [os.environ.get("FQDN")]
 
 DEBUG = False
 
 SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
 
 setup_sentry()
-
-
-# if ZAPPA_STAGE == "prod":
-#     AWS_CLOUD_FRONT_ID = "E3E7Y3O7VH1EMS"
-# else:
-#     AWS_CLOUD_FRONT_ID = "E19EM1B0PXPYCH"
