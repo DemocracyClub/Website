c654785ee464b4337998cf0a5abea3378ff2bce7
Sym Roe <sym.roe@talusdesign.co.uk>
2021-09-30 17:31:26 +0100

    Add management_handler

--- democracy_club/lambda_awsgi.py
+++ democracy_club/lambda_awsgi.py
@@ -1,5 +1,9 @@
-import awsgi
+import json
 
+import awsgi
+import django
+import sentry_sdk
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
@@ -12,3 +16,46 @@ def lambda_handler(event, context):
         context,
         base64_content_types={"image/png", "image/jpeg", "image/x-icon"},
     )
+
+
+def management_handler(event, context):
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+
+    cmd = event["command"]
+    args = event.get("args", [])
+
+    sentry_sdk.set_context("event", event)
+
+    django.setup()
+
+    print(f"Calling {cmd} with args {args}")
+    call_command(cmd, *args)
+
+    arg_str = " ".join(args)
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "message": f"{cmd} {arg_str} completed",
+            }
+        ),
+    }
