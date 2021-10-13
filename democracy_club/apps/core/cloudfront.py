7efc42afff9a59afc0dc49d34e4ca9c2eac853a0
Sym Roe <sym.roe@talusdesign.co.uk>
2021-10-13 10:48:14 +0100

    Get the CloudFront ID automatically

--- democracy_club/apps/core/cloudfront.py
+++ democracy_club/apps/core/cloudfront.py
@@ -1,10 +1,10 @@
+import os
 from datetime import datetime
-
-from django.conf import settings
 
 
 def invalidate_paths(path_list):
-    if not hasattr(settings, "ZAPPA_STAGE"):
+    # Only run on Lambda
+    if not "AWS_EXECUTION_ENV" in os.environ:
         return
     # We don't always have boto, because we're not always on Lambda
     # and it's not a dependancy of this project. Fail nicely in this case.
@@ -14,8 +14,18 @@ def invalidate_paths(path_list):
         return
     cloudfront = boto3.client("cloudfront")
 
+    # Find the dist ARN for a given cname
+    dist_id = None
+    for dist in cloudfront.list_distributions()["DistributionList"]["Items"]:
+        cnames = [a["CNAME"] for a in dist["AliasICPRecordals"]]
+        if os.environ.get("FQDN") in cnames:
+            dist_id = dist["ARN"].split("/")[-1]
+
+    if not dist_id:
+        return
+
     cloudfront.create_invalidation(
-        DistributionId=settings.AWS_CLOUD_FRONT_ID,
+        DistributionId=dist_id,
         InvalidationBatch={
             "Paths": {"Quantity": len(path_list), "Items": path_list},
             "CallerReference": "django-invalidate-{}".format(datetime.now()),
