19520a109be6ca96a63d5de05a5d3b8b46aabfb6
Sym Roe <sym.roe@talusdesign.co.uk>
2021-11-15 12:54:56 +0000

    Allow public read by default on uploaded S3 objects

--- democracy_club/settings/aws_lambda.py
+++ democracy_club/settings/aws_lambda.py
@@ -41,6 +41,7 @@ AWS_S3_SECURE_URLS = True
 AWS_S3_HOST = "s3-eu-west-2.amazonaws.com"
 AWS_S3_USE_SSL = False
 AWS_S3_REGION_NAME = "eu-west-2"
+AWS_DEFAULT_ACL = "public-read"
 
 AWS_STORAGE_BUCKET_NAME = os.environ.get("STORAGE_BUCKET_NAME")
 AWS_S3_CUSTOM_DOMAIN = os.environ.get("FQDN")
