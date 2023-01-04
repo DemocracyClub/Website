6eec140dc32e162d8f7b88b45fdd85e3b18d6fd1
Virginia Dooley <vcdooley@gmail.com>
2023-01-04 23:39:25 +0000

    Add Post filters

--- democracy_club/apps/hermes/admin.py
+++ democracy_club/apps/hermes/admin.py
@@ -11,6 +11,7 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ["subject"]
     search_help_text = "Search by subject (Post title)"
     list_display = ["subject", "created_on", "category"]
+    list_filter = ["is_published", "author"]
 
 
 class CategoryAdmin(admin.ModelAdmin):
