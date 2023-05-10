8fc26a6d217c9bca2214d08ce0c5931875a67884
Sym Roe <sym.roe@talusdesign.co.uk>
2023-05-10 17:13:56 +0100

    Remove rest and textile from blog

--- democracy_club/apps/hermes/renderers.py
+++ democracy_club/apps/hermes/renderers.py
@@ -2,15 +2,3 @@ def markdown(markup):
     import markdown as md
 
     return md.markdown(markup)
-
-
-def restructured_text(markup):
-    from docutils import core
-
-    return core.publish_parts(markup, writer_name="html")["fragment"]
-
-
-def textile(markup):
-    import textile as tex
-
-    return tex.textile(markup, html_type="html")
