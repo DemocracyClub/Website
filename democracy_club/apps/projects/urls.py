6974abbd7314ae841d792f8f91c2dbcaf260e738
Sym Roe <sym.roe@talusdesign.co.uk>
2023-07-20 10:17:16 +0100

    fixup! Move reports to a single app

--- democracy_club/apps/projects/urls.py
+++ democracy_club/apps/projects/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from django.urls import reverse_lazy
 from django.views.generic import RedirectView, TemplateView
-from core.report_helpers.views import MarkdownFileView
 
 app_name = "projects"
 
