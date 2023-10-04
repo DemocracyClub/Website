e43ab068b0503e1e9409f2a5a20b89dd0ad5f68e
Virginia Dooley <vcdooley@gmail.com>
2023-10-04 15:40:11 +0100

    Render case_study posts

--- democracy_club/apps/hermes/templatetags/post_tags.py
+++ democracy_club/apps/hermes/templatetags/post_tags.py
@@ -4,11 +4,12 @@ from hermes.models import Post
 register = template.Library()
 
 
-def posts_for_tag(tag):
+def posts_for_tag(tag, hide_header=False):
     posts = Post.objects.for_tag(tag)[:4]
     return {
         "posts": posts,
         "tag": tag,
+        "hide_header": hide_header,
     }
 
 
