92b13acfe02f54e6ff405747dfdfd2c053e5c833
Virginia Dooley <vcdooley@gmail.com>
2023-10-12 10:14:33 +0100

    Add validation for case_studies page

--- democracy_club/apps/core/signals.py
+++ democracy_club/apps/core/signals.py
@@ -8,4 +8,4 @@ from hermes.models import Post
 def blog_url_invalidation_handler(sender, **kwargs):
     instance = kwargs["instance"]
     path = instance.get_absolute_url()
-    invalidate_paths([path, "/blog/", "/"])
+    invalidate_paths([path, "/blog/", "/", "/research/case_studies/"])
