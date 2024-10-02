790adf411cee219b746760f7fe36c0407583f8ad
awdem <wdemontmollin@gmail.com>
2024-10-02 11:12:50 +0100

    adds BasicAuth Middleware

--- democracy_club/settings/base.py
+++ democracy_club/settings/base.py
@@ -72,6 +72,7 @@ MIDDLEWARE = (
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "dc_utils.middleware.BasicAuthMiddleware",
 )
 
 
